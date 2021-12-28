60e195ba0cdf77b2a29e1d8f32ae7245c6465da7
PoLLaziY <xlanzerx@gmail.com>
2021-12-28 15:28:52 +0300

    End commit

--- src/Pawn.java
+++ src/Pawn.java
@@ -8,12 +8,12 @@ public class Pawn extends ChessPiece {
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int startLine, int startColumn, int toLine, int toColumn) {
         if (super.canMoveToPosition(chessBoard, startLine, startColumn, toLine, toColumn)) {
-            boolean start = (startColumn == toColumn) &&
+            boolean start = (startColumn == toColumn && chessBoard.board[toLine][toColumn] == null) &&
                     ((this.color.equals("White") && startLine == 1 && (toLine == 2 ||
-                            (toLine == 3 && chessBoard.board[3][toColumn] == null))) ||
+                            (toLine == 3 && chessBoard.board[2][toColumn] == null))) ||
                             (this.color.equals("Black") && startLine == 6 &&
                                     (toLine == 5 || (toLine == 4 && chessBoard.board[5][toColumn] == null))));
-            boolean notStart = (startColumn == toColumn) &&
+            boolean notStart = (startColumn == toColumn && chessBoard.board[toLine][toColumn] == null) &&
                     ((this.color.equals("White") && toLine == startLine + 1) ||
                             (this.color.equals("Black") && toLine == startLine - 1));
             boolean attack = (chessBoard.board[toLine][toColumn] != null) &&
