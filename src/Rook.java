266bb203adeeb5669034758caf4ef1a70d92dba6
PoLLaziY <xlanzerx@gmail.com>
2022-01-03 01:37:49 +0300

    Bishop

--- src/Rook.java
+++ src/Rook.java
@@ -9,7 +9,7 @@ public class Rook extends ChessPiece {
         if (!(super.canMoveToPosition(chessBoard, startLine, startColumn, toLine, toColumn) &&
                 (startLine == toLine || startColumn == toColumn))) return false;
         if (startLine == toLine) {
-            for (int i = startColumn; i < toColumn; i += (toColumn - startColumn) / Math.abs(toColumn - startColumn)) {
+            for (int i = startColumn; i != toColumn; i += (toColumn - startColumn) / Math.abs(toColumn - startColumn)) {
                 if (i == startColumn) continue;
                 if (!(chessBoard.board[toLine][i] == null)) {
                     return false;
@@ -17,7 +17,7 @@ public class Rook extends ChessPiece {
             }
             return true;
         } else {
-            for (int i = startLine; i < toLine; i += (toLine - startLine) / Math.abs(toLine - startLine)) {
+            for (int i = startLine; i != toLine; i += (toLine - startLine) / Math.abs(toLine - startLine)) {
                 if (i == startLine) continue;
                 if (!(chessBoard.board[i][toColumn] == null)) {
                     return false;
